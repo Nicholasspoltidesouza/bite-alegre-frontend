b65697f2681c7adab18f02799229bdb69fef5759
RafaelRothmann <rafinharothmann@gmail.com>
2025-03-25 16:10:36 -0300

    Creating the component

---
+++ gastro/src/components/HeaderPerfilRestaurante/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const HeaderPerfilRestaurante = ({ }) => {
+  return (
+    
+  );
+};
+
+export default HeaderPerfilRestaurante;
+
+
+
+
+// import { View, Text, StyleSheet } from 'react-native';
+
+// const HeaderPerfilRestaurante = ({ mensagem }) => {
+//   return (
+//     // <View style={styles.container}>
+//     //   <Text style={styles.texto}>{mensagem}</Text>
+//     // </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     padding: 16,
+//     backgroundColor: '#f0f0f0',
+//     borderRadius: 8,
+//     margin: 8,
+//   },
+//   texto: {
+//     fontSize: 16,
+//     color: '#333',
+//   },
+// });
