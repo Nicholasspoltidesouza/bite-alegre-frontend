fef3f1236a4b8a28990202e2f4c9d5f9162e6d62
rafael.rothmann <r.rothmann@edu.pucrs.br>
2025-03-26 18:41:56 -0300

    Adding Pin to the component

--- gastro/src/components/HeaderPerfilRestaurante/index.tsx
+++ gastro/src/components/HeaderPerfilRestaurante/index.tsx
@@ -1,16 +1,37 @@
 import React from 'react';
 import { View, ImageBackground, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 const HeaderPerfilRestaurante = ({ }) => {
   return (
-    <h1>Header Perfil do Restaurante</h1>
+    <div style={styles.container}>
+      <div style={styles.bola}>
+        <AntDesign name="pushpin" size={24} color="#FF770025" style={styles.icon} />
+      </div>
+    </div>
   );
 };
 
-// const styles = StyleSheet.create({
-
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0', // Cor de fundo da tela
+  },
+  bola: {
+    width: 50,            // Largura da bola (pequena)
+    height: 50,           // Altura da bola (pequena)
+    borderRadius: 25,     // Para tornar o "View" redondo
+    backgroundColor: 'white', // Cor branca
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  icon: {
+    position: 'absolute',
+  }
+});
 
 export default HeaderPerfilRestaurante;
 
