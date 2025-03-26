5b420af7014dac77a6fdd9d3c4b2640b0e13e03e
RafaelRothmann <rafinharothmann@gmail.com>
2025-03-26 15:36:35 -0300

    Adding styles to the component

--- gastro/src/components/HeaderPerfilRestaurante/index.tsx
+++ gastro/src/components/HeaderPerfilRestaurante/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { View, ImageBackground, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
-const HeaderPerfilRestaurante = ({ }) => {
+const HeaderPerfilRestaurante = ({ urlFotoDoBanner, urlFotoDoPerfil}) => {
   return (
-    
+
   );
 };
+
+const styles = StyleSheet.create({
+
+});
 
 export default HeaderPerfilRestaurante;
 
