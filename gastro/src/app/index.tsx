3e0e1f9c5028307c5f26003262b95b7f137bb90e
RafaelRothmann <rafinharothmann@gmail.com>
2025-03-27 01:05:10 -0300

    Adding a Profile Picture to the Header

--- gastro/src/app/index.tsx
+++ gastro/src/app/index.tsx
@@ -24,40 +24,11 @@ const TextInputExample: React.FC = () => {
 
   return (
 
-    <HeaderPerfilRestaurante urlFotoBanner={'https://media.discordapp.net/attachments/842552823412949003/1354653738295627936/image.png?ex=67e6131b&is=67e4c19b&hm=7427015e6d1f2178b1fb60e47b4ec1796d30bac349a019571ef36bbeafb3be85&=&format=webp&quality=lossless'}
-    
+    <HeaderPerfilRestaurante 
+    urlFotoBanner={'https://media.discordapp.net/attachments/842552823412949003/1354653738295627936/image.png?ex=67e6131b&is=67e4c19b&hm=7427015e6d1f2178b1fb60e47b4ec1796d30bac349a019571ef36bbeafb3be85&=&format=webp&quality=lossless'}
+    urlFotoPerfil={''}
     ></HeaderPerfilRestaurante>
 
-    // <View style={styles.container}>
-    //   <CustomTextInput
-    //     value={name}
-    //     onChangeText={setName}
-    //     placeholder="Nome"
-    //     style={styles.customInput}
-    //     validation={validateName}
-    //   />
-    //   <CustomTextInput
-    //     value={email}
-    //     onChangeText={setEmail}
-    //     placeholder="Email"
-    //     style={styles.customInput}
-    //     validation={validateEmail}
-    //   />
-    // </View>  <CustomTextInput
-    //     value={name}
-    //     onChangeText={setName}
-    //     placeholder="Nome"
-    //     style={styles.customInput}
-    //     validation={validateName}
-    //   />
-    //   <CustomTextInput
-    //     value={email}
-    //     onChangeText={setEmail}
-    //     placeholder="Email"
-    //     style={styles.customInput}
-    //     validation={validateEmail}
-    //   />
-    // </View>
   );
 };
 
--- gastro/src/components/HeaderPerfilRestaurante/index.tsx
+++ gastro/src/components/HeaderPerfilRestaurante/index.tsx
@@ -1,52 +1,170 @@
 import React from 'react';
-import { StyleSheet, ImageBackground, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, ImageBackground, TouchableOpacity, View, Image } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 
 interface HeaderPerfilRestauranteProps {
   urlFotoBanner: string;
+  urlFotoPerfil: string;
 }
 
-const HeaderPerfilRestaurante: React.FC<HeaderPerfilRestauranteProps> = ({ urlFotoBanner }) => {
+const HeaderPerfilRestaurante: React.FC<HeaderPerfilRestauranteProps> = ({ urlFotoBanner, urlFotoPerfil }) => {
   return (
-    <ImageBackground source={{ uri: urlFotoBanner }} style={styles.container} imageStyle={styles.imageBackground}>
-      <TouchableOpacity style={styles.bola}>
-        <AntDesign name="pushpin" size={24} color="#FF770040" style={styles.icon} />
-      </TouchableOpacity>
-    </ImageBackground>
+    <View style={styles.container_banner}>
+      <ImageBackground source={{ uri: urlFotoBanner }} style={styles.container} imageStyle={styles.imageBackground}>
+        <TouchableOpacity style={styles.pinButton}>
+          <AntDesign name="pushpin" size={24} color="#FF770040" style={styles.icon} />
+        </TouchableOpacity>
+      </ImageBackground>
+      
+      <View style={styles.profileContainer}>
+        {urlFotoPerfil ? (
+          <Image source={{ uri: urlFotoPerfil }} style={styles.profileImage} />
+        ) : (
+          <FontAwesome6 name="user-large" size={40} color="#FFF" style={styles.profileIcon} />
+        )}
+      </View>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container_banner: {
+    width: '100%',
+    alignItems: 'center',
+  },
   container: {
     width: '100%',
     height: 200,
     justifyContent: 'flex-start',
     alignItems: 'flex-end',
-    padding: 10,
+    padding: 15,
     backgroundColor: '#FF914B',
-    borderBottomLeftRadius: 25,
-    borderBottomRightRadius: 25
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
   },
   imageBackground: {
-    borderRadius: 10,
-    overflow: 'hidden',
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
   },
-  bola: {
-    width: 50,
-    height: 50,
-    borderRadius: 25,
+  pinButton: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
     backgroundColor: 'white',
     justifyContent: 'center',
     alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    shadowRadius: 4,
-    elevation: 5,
   },
   icon: {
     transform: [{ rotate: '90deg' }], 
   },
+  profileContainer: {
+    width: 90,
+    height: 90,
+    borderRadius: 40,
+    backgroundColor: '#FFB370',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: -40,
+    borderColor: '#FFB370',
+    overflow: 'hidden',
+  },
+  profileImage: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 40,
+  },
+  profileIcon: {
+    alignSelf: 'center',
+  },
 });
 
 export default HeaderPerfilRestaurante;
+
+// import React from 'react';
+// import { StyleSheet, ImageBackground, TouchableOpacity } from 'react-native';
+// import { AntDesign } from '@expo/vector-icons';
+// import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+
+// interface HeaderPerfilRestauranteProps {
+//   urlFotoBanner: string;
+//   urlFotoPerfil: string;
+// }
+
+// const HeaderPerfilRestaurante: React.FC<HeaderPerfilRestauranteProps> = ({ urlFotoBanner, urlFotoPerfil }) => {
+//   return (
+//     <div style={styles.container_banner}>
+//       <ImageBackground source={{ uri: urlFotoBanner }} style={styles.container} imageStyle={styles.imageBackground}>
+//         <TouchableOpacity style={styles.bola}>
+//           <AntDesign name="pushpin" size={24} color="#FF770040" style={styles.icon} />
+//         </TouchableOpacity>
+//       </ImageBackground>
+
+//       <div style={styles.bola_perfil}>
+//         <FontAwesome6 name="user-large" size={24} color="#FFFDFC" style={styles.perfil} />
+//       </div>
+
+//     </div>
+
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     width: '100%',
+//     height: 200,
+//     justifyContent: 'flex-start',
+//     alignItems: 'flex-end',
+//     padding: 10,
+//     backgroundColor: '#FF914B',
+//     borderBottomLeftRadius: 25,
+//     borderBottomRightRadius: 25
+//   },
+//   imageBackground: {
+//     overflow: 'hidden',
+//     borderBottomLeftRadius: 25,
+//     borderBottomRightRadius: 25
+//   },
+//   bola: {
+//     width: 50,
+//     height: 50,
+//     borderRadius: 25,
+//     backgroundColor: 'white',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     shadowColor: '#000',
+//     shadowOffset: { width: 0, height: 2 },
+//     shadowOpacity: 0.2,
+//     shadowRadius: 4,
+//     elevation: 5,
+    
+//   },
+//   icon: {
+//     transform: [{ rotate: '90deg' }], 
+//   },
+//   bola_perfil: {
+//     backgroundColor: "black",
+//     width: 50,
+//     height: 50,
+//     borderRadius: 25,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     shadowColor: '#000',
+//     shadowOffset: { width: 0, height: 2 },
+//     shadowOpacity: 0.2,
+//     shadowRadius: 4,
+//     elevation: 5,
+//     display: 'flex',
+//     alignContent: 'center',
+//     position: 'absolute'
+//   },
+//   perfil: {
+//     alignItems: 'center'
+//   },
+//   container_banner: {
+//     display: 'flex'
+//   }
+// });
+
+// export default HeaderPerfilRestaurante;
