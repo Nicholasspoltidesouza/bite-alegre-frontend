946cbfc9ed2e7d039767676d330ad4916cb5363b
RafaelRothmann <rafinharothmann@gmail.com>
2025-03-26 23:56:55 -0300

    Adding a Banner to the Header Component for the Restaurant Profile

--- gastro/src/app/index.tsx
+++ gastro/src/app/index.tsx
@@ -24,7 +24,7 @@ const TextInputExample: React.FC = () => {
 
   return (
 
-    <HeaderPerfilRestaurante></HeaderPerfilRestaurante>
+    <HeaderPerfilRestaurante urlFotoBanner={''}></HeaderPerfilRestaurante>
 
     // <View style={styles.container}>
     //   <CustomTextInput
--- gastro/src/components/HeaderPerfilRestaurante/index.tsx
+++ gastro/src/components/HeaderPerfilRestaurante/index.tsx
@@ -1,38 +1,52 @@
 import React from 'react';
-import { View, ImageBackground, StyleSheet, Text, TouchableOpacity } from 'react-native';
-import { MaterialIcons } from '@expo/vector-icons';
-import AntDesign from '@expo/vector-icons/AntDesign';
+import { StyleSheet, ImageBackground, View, TouchableOpacity } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 
-const HeaderPerfilRestaurante = ({ }) => {
+interface HeaderPerfilRestauranteProps {
+  urlFotoBanner: string;
+}
+
+const HeaderPerfilRestaurante: React.FC<HeaderPerfilRestauranteProps> = ({ urlFotoBanner }) => {
   return (
-    <div style={styles.container}>
-      <button style={styles.bola}>
+    <ImageBackground source={{ uri: urlFotoBanner }} style={styles.container} imageStyle={styles.imageBackground}>
+      <TouchableOpacity style={styles.bola}>
         <AntDesign name="pushpin" size={24} color="#FF770040" style={styles.icon} />
-      </button>
-    </div>
+      </TouchableOpacity>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f0f0f0', 
+    width: '100%',
+    height: 200,
+    justifyContent: 'flex-start',
+    alignItems: 'flex-end',
+    padding: 10,
+    backgroundColor: '#FF914B',
+    borderBottomLeftRadius: 25,
+    borderBottomRightRadius: 25
+  },
+  imageBackground: {
+    borderRadius: 10,
+    overflow: 'hidden',
   },
   bola: {
-    width: 50,            
-    height: 50,           
-    borderRadius: 25,     
+    width: 50,
+    height: 50,
+    borderRadius: 25,
     backgroundColor: 'white',
     justifyContent: 'center',
     alignItems: 'center',
-    display: 'flex',
-    borderWidth: 0
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 5,
   },
   icon: {
-    alignItems: 'center'
-  }
+    transform: [{ rotate: '90deg' }],
+  },
 });
 
 export default HeaderPerfilRestaurante;
