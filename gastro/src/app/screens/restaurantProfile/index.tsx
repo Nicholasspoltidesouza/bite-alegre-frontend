efcca85949013b8a8ab7268c68f8a4746ce174f7
RafaelRothmann <rafinharothmann@gmail.com>
2025-04-01 00:01:47 -0300

    feat: Retirando o header da pagina inicial

--- gastro/src/app/screens/restaurantProfile/index.tsx
+++ gastro/src/app/screens/restaurantProfile/index.tsx
@@ -1,23 +1,16 @@
 import HeaderPerfilRestaurante from '@/src/components/HeaderPerfilRestaurante';
-import React = require('react');
-
-
 
 const restaurantProfile: React.FC = () =>{
   
   return (
 
     <HeaderPerfilRestaurante
-      urlFotoBanner={'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.tripadvisor.com.br%2FRestaurants-g303546-Porto_Alegre_State_of_Rio_Grande_do_Sul.html&psig=AOvVaw0rtvEJxkxhVbBmAkozIgu7&ust=1743543446232000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJjsttSjtYwDFQAAAAAdAAAAABAE'}
+      urlFotoBanner={'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/12/5a/28/0e/salao-principal.jpg?w=600&h=-1&s=1'}
       urlFotoPerfil={''}
     ></HeaderPerfilRestaurante>
 
   );
 
 }
-
-
-
- 
 
 export default restaurantProfile;
