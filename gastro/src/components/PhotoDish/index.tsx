b731bc51bdca4bd91596ecc93db47ac15b8f30cd
RafaelRothmann <rafinharothmann@gmail.com>
2025-04-03 16:19:25 -0300

    feat: Removido comentarios

--- gastro/src/components/PhotoDish/index.tsx
+++ gastro/src/components/PhotoDish/index.tsx
@@ -22,7 +22,7 @@ const PhotoDish: React.FC<PhotoDishProps> = ({ urlFotoPrato, descricao, size = 2
             shadowOpacity: 0.3,
             shadowRadius: 5,
             elevation: 5,
-            position: 'relative', // Added for star positioning
+            position: 'relative',
         },
         card: {
             width: size,
@@ -110,102 +110,3 @@ const PhotoDish: React.FC<PhotoDishProps> = ({ urlFotoPrato, descricao, size = 2
 };
 
 export default PhotoDish;
-
-// import React, { useRef } from "react";
-// import { View, StyleSheet, Text, Image, Animated, TouchableOpacity } from "react-native";
-
-// interface PhotoDishProps {
-//     urlFotoPrato: string;
-//     descricao: string;
-//     size?: number;
-// }
-
-// const PhotoDish: React.FC<PhotoDishProps> = ({ urlFotoPrato, descricao, size = 200 }) => {
-//     const flipAnim = useRef(new Animated.Value(0)).current;
-//     let isFlipped = false;
-
-//     const styles = StyleSheet.create({
-//         container: {
-//             borderRadius: 20,
-//             overflow: 'hidden',
-//             shadowColor: '#000',
-//             shadowOffset: { width: 0, height: 4 },
-//             shadowOpacity: 0.3,
-//             shadowRadius: 5,
-//             elevation: 5,
-//         },
-//         card: {
-//             width: size,
-//             height: size,
-//             backfaceVisibility: 'hidden',
-//         },
-//         image: {
-//             borderRadius: 20,
-//             resizeMode: 'cover',
-//         },
-//         backCard: {
-//             position: 'absolute',
-//             top: 0,
-//             width: '100%',
-//             height: '100%',
-//             justifyContent: 'center',
-//             alignItems: 'center',
-//             backgroundColor: '#f8f8f8',
-//             borderRadius: 20,
-//             padding: 15,
-//         },
-//         descriptionText: {
-//             fontSize: 16,
-//             textAlign: 'center',
-//             color: '#333',
-//         },
-//     });
-
-//     const flipCard = () => {
-//         Animated.spring(flipAnim, {
-//             toValue: isFlipped ? 0 : 180,
-//             friction: 8,
-//             tension: 10,
-//             useNativeDriver: true,
-//         }).start(() => {
-//             isFlipped = !isFlipped;
-//         });
-//     };
-
-//     const frontInterpolate = flipAnim.interpolate({
-//         inputRange: [0, 180],
-//         outputRange: ['0deg', '180deg'],
-//     });
-
-//     const backInterpolate = flipAnim.interpolate({
-//         inputRange: [0, 180],
-//         outputRange: ['180deg', '360deg'],
-//     });
-
-//     const frontAnimatedStyle = {
-//         transform: [{ rotateY: frontInterpolate }],
-//     };
-
-//     const backAnimatedStyle = {
-//         transform: [{ rotateY: backInterpolate }],
-//     };
-
-//     return (
-//         <TouchableOpacity activeOpacity={0.9} onPress={flipCard}>
-//             <View style={[styles.container, { width: size, height: size }]}>
-//                 <Animated.View style={[styles.card, frontAnimatedStyle]}>
-//                     <Image 
-//                         source={{ uri: urlFotoPrato }} 
-//                         style={[styles.image, { width: size, height: size }]} 
-//                     />
-//                 </Animated.View>
-                
-//                 <Animated.View style={[styles.card, styles.backCard, backAnimatedStyle]}>
-//                     <Text style={styles.descriptionText}>{descricao}</Text>
-//                 </Animated.View>
-//             </View>
-//         </TouchableOpacity>
-//     );
-// };
-
-// export default PhotoDish;
