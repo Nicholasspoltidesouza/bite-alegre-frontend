ac162b4ee53a99c18bc737e67288278b0ac42cb9
João Vitor Krampe Schwening <joaovitorkrampe@vortigo.com.br>
2025-04-05 16:57:27 -0300

    feature/US-04-task18-Tela-Cadastro-restaurante: desenvolvendo a tela de cadastro do restaurante e resolvendo problemas da tela

--- gastro/src/hooks/useRestaurantApi.ts
+++ gastro/src/hooks/useRestaurantApi.ts
@@ -1,21 +1,12 @@
 import { useState } from "react";
-import { API_URL_BACKEND } from "../constants/apiUrl";
 
-interface Restaurant {
-  id: string;
-  profilePhoto: string;
-  bannerPhoto: string;
-  name: string;
-  description: string;
-  address: string;
-  averagePrice: string;
-  phone: string;
-}
+import { API_URL_BACKEND } from "../constants/apiUrl"; //cria apiUrl
+import { RestaurantDTO } from "../@types/DTO";
 
 export const useRestaurantApi = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
-  const [data, setData] = useState<Restaurant | null>(null);
+  const [data, setData] = useState<RestaurantDTO | null>(null);
 
   const getRestaurant = async (restaurantId: string): Promise<void> => {
     setLoading(true);
@@ -47,5 +38,37 @@ export const useRestaurantApi = () => {
     }
   };
 
-  return { getRestaurant, loading, error, data };
+  const createRestaurant = async (restaurantData: RestaurantDTO): Promise<void> => {
+    setLoading(true);
+    setError(null);
+
+    try {
+        const response = await fetch (`${API_URL_BACKEND}/restaurants`, {
+            method: "POST",
+            headers: {
+                "Contend-Type": "application/json",
+            },
+            body: JSON.stringify(restaurantData),
+        });
+        const responseData = await response.json();
+
+        if(response.ok) {
+            console.log("Usuário criado:", responseData);
+            setData(responseData);
+        } else {
+            throw new Error(
+                responseData.error ||
+                responseData.message ||
+                `Falha ao criar restaurante. Status: ${response.status}`,
+            );
+        }
+    } catch (err: any) {
+        console.error("Erro ao criar restaurante:", err);
+        setError(err.message || "Erro desconhecido");
+    } finally {
+        setLoading(false);
+    }
+};
+
+  return { createRestaurant, getRestaurant, loading, error, data };
 };
