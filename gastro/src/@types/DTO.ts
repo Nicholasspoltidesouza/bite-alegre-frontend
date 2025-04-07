04ed545b74112bb1e82a9c0db4155b48eff4522c
João Vitor Krampe Schwening <joaovitorkrampe@vortigo.com.br>
2025-04-07 16:23:24 -0300

    feature/US-04-task18-Tela-Cadastro-restaurante: desenvolvendo a tela de cadastro do restaurante, terminei a função onPressItem que torna os itens de dentro do hoursSection clicáveis

--- gastro/src/@types/DTO.ts
+++ gastro/src/@types/DTO.ts
@@ -1,13 +1,11 @@
-interface RestaurantDTO {
+export interface RestaurantDTO {
     profilePhoto?: string;
     bannerPhoto?: string;
     address: string;
-    name: string;
+    restaurantName: string;
     email: string;
     password: string;
     averagePrice: number;
     phone: string;
     restaurantType: string | null;
 }
-
-export type { RestaurantDTO };
