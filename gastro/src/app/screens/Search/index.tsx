65f6af86bbb9ba8be3511ec3ae3e9e83105d233c
caiqueSteibel <caique.steibel@edu.pucrs.br>
2025-04-16 18:44:24 -0300

    feat: add tela de busca

--- gastro/src/app/screens/Search/index.tsx
+++ gastro/src/app/screens/Search/index.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import CustomTextInput from '@/src/components/CustomTextInput';
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 
-export default function Search() {
+const Search = () => {
+  const [search, setSearch] = useState<string>('');
+
+  const validateSearch = (text: string): string | null => {
+    return null;
+  };
+
   return (
     <View>
-      <Text>Tela de pesquisa</Text>
+      <CustomTextInput
+        value={search}
+        onChangeText={setSearch}
+        placeholder="Pesquisar"
+        style={styles.input}
+        validation={validateSearch}
+      />
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  input: {
+    width: '100%',
+    height: 50,
+    borderRadius: 20,
+    backgroundColor: "rgba(255, 179, 112, 0.25)",
+    paddingLeft: 24,
+    paddingRight: 16,
+    color: "#000000",
+    fontFamily: "Poppins-Regular",
+    fontSize: 16,
+  },
+});
+
+export default Search;
--- gastro/src/app/screens/SignupRestaurant/index.tsx
+++ gastro/src/app/screens/SignupRestaurant/index.tsx
@@ -1,9 +1,9 @@
 import { RestaurantDTO } from '@/src/@types/DTO';
 import { OperatingHoursDto } from '@/src/@types/OperatingHoursDto';
 import Button from '@/src/components/Button';
+import CustomTextInput from '@/src/components/CustomTextInput';
 import HoursSection from '@/src/components/HoursSection';
 import SignupHeader from '@/src/components/SignupHeader';
-import CustomTextInput from '@/src/components/TextFieldCadastroUsuario';
 import { useRestaurantApi } from '@/src/hooks/useRestaurantApi';
 import { useRouter } from 'expo-router';
 import React, { useState } from 'react';
@@ -152,7 +152,7 @@ const SignupRestaurant = () => {
 
       await createRestaurant(restaurantData);
       Alert.alert('Sucesso', 'Restaurante cadastrado com sucesso!');
-      router.push({ pathname: "/screens/SignupInterestsScreen", params: { screenTitle: "Selecione as categorias do seu restaurante", backRoute: "/screens/SignupRestaurant"} });
+      router.push({ pathname: "/screens/SignupInterestsScreen", params: { screenTitle: "Selecione as categorias do seu restaurante", backRoute: "/screens/SignupRestaurant" } });
     } catch (err) {
       console.error('Submit Error:', err);
       Alert.alert('Erro', err instanceof Error ? err.message : 'Ocorreu um erro inesperado');
@@ -193,7 +193,7 @@ const SignupRestaurant = () => {
               validation={validateNameRestaurant}
             />
           </View>
-          
+
           <View style={styles.inputWrapper}>
             <CustomTextInput
               value={description}
@@ -206,7 +206,7 @@ const SignupRestaurant = () => {
               textAlignVertical="top"
             />
           </View>
-          
+
           <View style={styles.inputWrapper}>
             <CustomTextInput
               value={address}
@@ -217,7 +217,7 @@ const SignupRestaurant = () => {
               autoCapitalize="none"
             />
           </View>
-          
+
           <View style={styles.inputWrapper}>
             <CustomTextInput
               value={email}
@@ -229,7 +229,7 @@ const SignupRestaurant = () => {
               autoCapitalize="none"
             />
           </View>
-          
+
           <View style={styles.inputWrapper}>
             <CustomTextInput
               value={password}
@@ -240,7 +240,7 @@ const SignupRestaurant = () => {
               secureTextEntry
             />
           </View>
-          
+
           <View style={styles.inputWrapper}>
             <CustomTextInput
               value={averagePrice}
@@ -252,7 +252,7 @@ const SignupRestaurant = () => {
               autoCapitalize="none"
             />
           </View>
-          
+
           <View style={styles.inputWrapper}>
             <CustomTextInput
               value={phone}
@@ -271,7 +271,7 @@ const SignupRestaurant = () => {
               maxLength={15}
             />
           </View>
-          
+
           <View style={styles.inputWrapper}>
             <HoursSection
               hours={operatingHours}
@@ -279,7 +279,7 @@ const SignupRestaurant = () => {
               onPressItem={handleEditOperatingHour}
             />
           </View>
-          
+
           <View style={styles.buttonContainer}>
             <Button title="Avançar" type="orange" onPress={handleSubmit} disabled={!isFormValid} />
           </View>
