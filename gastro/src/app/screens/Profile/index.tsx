12d32dc4ca0c7f75a4dab3f1527eba26aecaafef
Enzo <e.pecantet@edu.pucrs.br>
2025-04-17 17:02:53 -0300

    feat: feito carrosel, com botao de fixado e suas avaliacoesã

--- gastro/src/app/screens/Profile/index.tsx
+++ gastro/src/app/screens/Profile/index.tsx
@@ -5,12 +5,11 @@ import { View, Text } from 'react-native';
 
 export default function Profile() {
   return (
-    <View>
+    <View style={{ paddingHorizontal: 16, paddingTop: 20 }}>
+      <Text style={{ fontSize: 18, fontWeight: 'bold', color: '#FF914B', marginBottom: 10 }}>
+      
+      </Text>
       <UserCarouselRestaurant />
-
-     
-  
-
     </View>
   );
-} 
+}
