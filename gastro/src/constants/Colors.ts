e6853afe67829e977432c69a95ecbcf68b879a12
Enzo <e.pecantet@edu.pucrs.br>
2025-04-17 22:30:28 -0300

    feat: finalizado as constantes de cores do projeto.

--- gastro/src/constants/Colors.ts
+++ gastro/src/constants/Colors.ts
@@ -3,38 +3,11 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
-
-export const Colors = {
-
-  light: {
-    // light: {
+const Colors = {
     text: '#11181C',
     background: '#fff',
-    tint: tintColorLight,
     icon: '#687076',
     tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
-    backgroundCustom: '#FFFDFC',
-    padrao: '#FF914B',
-    pesquisar: '#FFFDFC',
-    historico: '#EAEAE',
-    icons: '#04565A',
-    verdeClaro: '#92FFC3',
-    amareloClaro: '#FFCC92',
-    laranjaVivo: '#FF7700',
-    azulClarinho: '#06C1B1',
-  },
-
-  dark: {
-    // dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
     backgroundCustom: '#FFFDFC',
     padrao: '#FF914B',
     pesquisar: '#FFFDFC',
@@ -44,5 +17,6 @@ export const Colors = {
     amareloClaro: '#FFCC92',
     laranjaVivo: '#FF7700',
     azulClaro: '#06C1B1',
-  },
+  
 }; 
+export default Colors;
