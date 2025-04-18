b10a8b62f3faf27dfe4c2a3ffc828764610ef53c
Enzo <e.pecantet@edu.pucrs.br>
2025-04-18 13:49:40 -0300

    feat: aqui ele indentifica atraves de boolean se o restaurante ele foi salvo ou visitado, tambem se o avaliacao foi null, aparece o botao para avaliar, tambem colocar as estrelas conforme o numero da avaliacao.

--- gastro/src/app/screens/Profile/index.tsx
+++ gastro/src/app/screens/Profile/index.tsx
@@ -1,15 +1,44 @@
-import PhotoDish from '@/src/components/PhotoDish';
 import UserCarouselRestaurant from '@/src/components/UserCarouselRestaurant';
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 export default function Profile() {
   return (
-    <View style={{ paddingHorizontal: 16, paddingTop: 20 }}>
-      <Text style={{ fontSize: 18, fontWeight: 'bold', color: '#FF914B', marginBottom: 10 }}>
-      
-      </Text>
-      <UserCarouselRestaurant />
+    <View style={styles.container}>
+
+      <View style={styles.titleRow}>
+        <Text style={styles.title}>Visitados</Text>
+        <TouchableOpacity>
+          <Text style={styles.mostrarMais}>Mostrar mais</Text>
+        </TouchableOpacity>
+      </View>
+      <UserCarouselRestaurant mostrarVisitados />
+
+      <Text style={[styles.title, { marginTop: 24 }]}>Salvos</Text>
+      <UserCarouselRestaurant mostrarVisitados = {false} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 16,
+    paddingTop: 20,
+  },
+  titleRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#FF914B',
+  },
+  mostrarMais: {
+    fontSize: 13,
+    fontWeight: '600',
+    color: '#FF914B',
+  },
+});
