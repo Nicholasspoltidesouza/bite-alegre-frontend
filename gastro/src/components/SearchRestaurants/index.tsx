468e8a51544786820aa5c9d2198c614977e2b549
João Vitor Krampe Schwening <joaovitorkrampe@gmail.com>
2025-04-19 13:06:52 -0300

    Desenvolvendo o componente SearchRestaurants

--- gastro/src/components/SearchRestaurants/index.tsx
+++ gastro/src/components/SearchRestaurants/index.tsx
@@ -1,9 +1,35 @@
 import React from "react"
 import { View, Text } from "react-native";
+import { MaterialIcons } from '@expo/vector-icons';
 
-const SearchRestaurants: React.FC = () => {
+
+interface SearchRestaurantsProps {
+    name : string;
+    averagePrice : number;
+    note : number;
+    location : string;
+    profilePhoto? : string
+}
+
+const SearchRestaurants: React.FC<SearchRestaurantsProps> = ({name, averagePrice, note, location, profilePhoto}) => {
+    const iconSize = 24;
+    const iconLeft = 18;
+    const iconRight = 8;
+    const spacing = 8;
+
+    const dynamicPaddingLeft = iconLeft + iconSize + spacing;
+    const dynamicPaddingRight = iconRight + iconSize + spacing;
+    
     return (
     <View>
+        <View>
+            profilePhoto={profilePhoto}
+            name={name}
+            averagePrice={averagePrice}
+            <MaterialIcons name="star" size={iconSize} color="#FF914B" />
+            note={note}
+            location={location}
+        </View>
         <Text>Componente de pesquisa de restaurantes</Text>
 
     </View>
--- gastro/src/components/SearchUsers/index.tsx
+++ gastro/src/components/SearchUsers/index.tsx
@@ -1,6 +1,12 @@
 import React from "react"
 import { View, Text } from "react-native";
 
+interface SearchUsers {
+    name : string;
+    nickname : string;
+    profilePhoto : string;
+}
+
 const SearchUsers: React.FC = () => {
     return (
     <View>
