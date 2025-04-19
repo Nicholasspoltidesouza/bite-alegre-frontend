d4d35a090525db0af2e1090f32fc3439ae5211d1
João Vitor Krampe Schwening <joaovitorkrampe@gmail.com>
2025-04-19 12:41:16 -0300

    Criando o componente SearchRestaurants e SearchUsers

---
+++ gastro/src/components/SearchRestaurants/index.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { View, Text } from "react-native";
+
+const SearchRestaurants: React.FC = () => {
+    return (
+    <View>
+        <Text>Componente de pesquisa de restaurantes</Text>
+
+    </View>
+    )
+}
+
+export default SearchRestaurants
---
+++ gastro/src/components/SearchUsers/index.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { View, Text } from "react-native";
+
+const SearchUsers: React.FC = () => {
+    return (
+    <View>
+        <Text>Componente de pesquisa de usuários</Text>
+
+    </View>
+    )
+}
+
+export default SearchUsers
