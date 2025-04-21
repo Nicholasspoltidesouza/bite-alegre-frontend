6472d624028b0354f4091f9251dd567c5c23484f
Nicholas Spolti <nicholasspoltidesouza@gmail.com>
2025-04-21 15:15:07 -0300

    troca do app

--- gastro/src/app/index.tsx
+++ gastro/src/app/index.tsx
@@ -1,20 +1,34 @@
-import React from "react"
-import { StyleSheet, View } from "react-native"
-import FilterScreen from "./screens/FilterScreen"
+import { router } from 'expo-router';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Button from '../components/Button';
+import Header from '../components/Header';
 
 const App: React.FC = () => {
+
   return (
     <View style={styles.container}>
-      <FilterScreen />
+      <Header />
+      <View style={styles.buttons}>
+        <Button title="Perfil Restaurante" onPress={() => router.push({ pathname: '/screens/restaurantProfile' })} type={'orange'} />
+        <Button title="Cadastro Usuário" onPress={() => router.push({ pathname: '/screens/SignupUser' })} type={'orange'} />
+        <Button title="Cadastro Restaurante" onPress={() => router.push({ pathname: '/screens/SignupRestaurant' })} type={'orange'} />
+      </View>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#FFFFFF",
+    backgroundColor: '#FFFFFF',
   },
-})
+  buttons: {
+    flex: 1,
+    marginTop: 15,
+    alignItems: 'center',
+    gap: 16,
+  }
+});
 
-export default App
+export default App;
