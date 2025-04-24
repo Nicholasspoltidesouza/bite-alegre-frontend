0c5c5af03f2f15f3dc667b528551bc8a404be11c
Enzo <e.pecantet@edu.pucrs.br>
2025-04-24 19:59:51 -0300

    feat: agora ajustei a tela do profile, por conta do rebase, meio que confundiu todo código, senão teria que ter criado outra branch.

--- gastro/src/app/screens/Profile/index.tsx
+++ gastro/src/app/screens/Profile/index.tsx
@@ -1,3 +1,4 @@
+import UserCarousel from '@/src/components/UserCarouselRestaurant';
 import UserCarouselRestaurant from '@/src/components/UserCarouselRestaurant';
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
