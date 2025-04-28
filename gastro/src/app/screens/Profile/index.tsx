88312271ec148dc1287c378ea94901438973ec8c
Enzo <e.pecantet@edu.pucrs.br>
2025-04-28 12:40:26 -0300

    feat: ajustei o rebase

--- gastro/src/app/screens/Profile/index.tsx
+++ gastro/src/app/screens/Profile/index.tsx
@@ -1,10 +1,13 @@
 
 import Colors from '@/src/constants/Colors';
+import Header from '@/src/components/Header';
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
+
 export default function Profile() {
   return (
+
     <View style={styles.container}>
       <Text style={{ color: Colors.icon ,fontSize: 40 }}>
         Tela de profile
