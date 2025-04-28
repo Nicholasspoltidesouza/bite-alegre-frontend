ff37e335c7bf69cb3da870abcf3434e576e1cbd5
Enzo <e.pecantet@edu.pucrs.br>
2025-04-28 20:55:41 -0300

    feat: Adicionado o botão TextField

--- gastro/src/app/screens/Feed/index.tsx
+++ gastro/src/app/screens/Feed/index.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import { View, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
-import TextField from '@/src/components/TextFieldCadastroUsuario';
-import Colors from '@/src/constants/Colors';
+import { View, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import Header from '@/src/components/Header';
-
 
 export default function TextFieldWithFilter() {
   return (
     <View style={styles.container}>
-    <Header/>
+      <Header />
 
-        <View >
-            <TextField value={'Pesquisar'} onChangeText={function (text: string): void {
-            throw new Error('Function not implemented.');
-          } } placeholder={''}       
-      />
-      <TouchableOpacity style={styles.iconButton}>
-        <Ionicons name="options" size={24}/>
-      </TouchableOpacity>
-    </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.inputWrapper}>
+          <TextInput
+            placeholder="Pesquisar"
+            placeholderTextColor="#FF914B"
+            style={styles.input}
+          />
+          <TouchableOpacity style={styles.iconButton}>
+            <Ionicons name="options" size={20} color="#FF914B" />
+          </TouchableOpacity>
+        </View>
+      </View>
+
     </View>
   );
 }
@@ -29,7 +30,25 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#FFFFFF',
   },
+  searchContainer: {
+    paddingHorizontal: 20,
+    marginTop: 20,
+  },
+  inputWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#FFF1E6', 
+    borderRadius: 999,
+    paddingHorizontal: 16,
+    paddingVertical: 12,
+  },
+  input: {
+    flex: 1,
+    fontSize: 18,
+    color: '#FF914B',
+    fontFamily: 'Poppins-Regular',
+  },
   iconButton: {
-    marginLeft: 10,
+    marginLeft: -50,
   },
 });
