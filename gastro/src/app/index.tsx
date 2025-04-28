730a532b7b602a7cc00bbf55911c25712b0bc69f
Enzo <e.pecantet@edu.pucrs.br>
2025-04-28 18:16:14 -0300

    feat: criando o botao para irmos para o feed

--- gastro/src/app/index.tsx
+++ gastro/src/app/index.tsx
@@ -10,6 +10,7 @@ const App: React.FC = () => {
     <View style={styles.container}>
       <Header />
       <View style={styles.buttons}>
+        <Button title="Feed" onPress={() => router.push({ pathname: '/screens/Profile' })} type={'orange'} />
         <Button title="Perfil Restaurante" onPress={() => router.push({ pathname: '/screens/restaurantProfile' })} type={'orange'} />
         <Button title="Cadastro Usuário" onPress={() => router.push({ pathname: '/screens/SignupUser' })} type={'orange'} />
         <Button title="Cadastro Restaurante" onPress={() => router.push({ pathname: '/screens/SignupRestaurant' })} type={'orange'} />
