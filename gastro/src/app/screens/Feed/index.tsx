d6b7ab8c875ae4d1be1a69b42bc981ab960b9871
Paulo Renato Kertész de Oliveira <paulo.renato@edu.pucrs.br>
2025-04-30 16:01:47 -0300

    Adicionando animação no banner da roleta e mini icone de roleta

--- gastro/src/app/screens/Feed/index.tsx
+++ gastro/src/app/screens/Feed/index.tsx
@@ -1,50 +1,82 @@
-import React from 'react';
-import { View, StyleSheet, TextInput, TouchableOpacity, ScrollView, Image, Text } from 'react-native';
+import React, { useRef } from 'react';
+import {View,StyleSheet,TextInput,TouchableOpacity,ScrollView,Image,Text,Animated,NativeSyntheticEvent,NativeScrollEvent} from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import Header from '@/src/components/Header';
 import UserCarouselRestaurant from '@/src/components/UserCarouselRestaurant';
 import Colors from '@/src/constants/Colors';
 
 export default function TextFieldWithFilter() {
+  const fadeAnim = useRef(new Animated.Value(1)).current;
+  const isHidden = useRef(false);
+
+  const handleScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+    const yOffset = event.nativeEvent.contentOffset.y;
+
+    if (yOffset > 50 && !isHidden.current) {
+      // fade out
+      Animated.timing(fadeAnim, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+      isHidden.current = true;
+    } else if (yOffset <= 50 && isHidden.current) {
+      // fade in
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+      isHidden.current = false;
+    }
+  };
+
   return (
-    <ScrollView>
-    <View style={styles.container}>
-      <Header />
-
-      <View style={styles.searchContainer}>
-        <View style={styles.inputWrapper}>
-          <TextInput
-            placeholder="Pesquisar"
-            placeholderTextColor="#FF914B"
-            style={styles.input}
-          />
-          <TouchableOpacity style={styles.iconButton}>
-            <Ionicons name="options" size={20} color="#FF914B" />
-          </TouchableOpacity>
+    <ScrollView onScroll={handleScroll} scrollEventThrottle={16}>
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.searchContainer}>
+          <View style={styles.inputWrapper}>
+            <TextInput
+              placeholder="Pesquisar"
+              placeholderTextColor="#FF914B"
+              style={styles.input}
+            />
+            <Image
+            source={require('@/assets/images/icon-roleta.png')}
+            style={styles.roulleteIcon}
+            />
+            <TouchableOpacity style={styles.iconButton}>
+              <Ionicons name="options" size={20} color="#FF914B" />
+            </TouchableOpacity>
+          </View>
         </View>
+
+        {/*Banner*/}
+        <Animated.View style={[styles.rouletteCardContainer, { opacity: fadeAnim },{height:fadeAnim.interpolate({inputRange:[0,1],outputRange:[0,200]})}]}>
+          <TouchableOpacity onPress={() => console.log('Card pressionado')}>
+            <Image
+              source={require('@/assets/images/card-roulette.png')}
+              style={styles.rouletteCard}
+              resizeMode="contain"
+            />
+          </TouchableOpacity>
+        </Animated.View>
+
+        <Text style={styles.title}>Em alta com nossos influencers</Text>
+        <UserCarouselRestaurant variant="influencers" />
+
+        <Text style={styles.title}>Restaurantes perto de você</Text>
+        <UserCarouselRestaurant variant="closeToYou" />
+        <Text style={styles.title}>Em alta com nossos influencers</Text>
+        <UserCarouselRestaurant variant="influencers" />
+
+        <Text style={styles.title}>Restaurantes perto de você</Text>
+        <UserCarouselRestaurant variant="closeToYou" />
+        
       </View>
-
-      <View style={styles.rouletteCardContainer}>
-        <TouchableOpacity onPress={() => console.log('Card pressionado')}>
-          <Image
-            source={require('@/assets/images/card-roulette.png')}
-            style={styles.rouletteCard}
-            resizeMode="contain"
-          />
-        </TouchableOpacity>
-      </View>
-      
-      <Text style={styles.title}>Em alta com nossos influencers</Text>
-
-      <UserCarouselRestaurant variant="influencers" />
-
-      <Text style={styles.title}>Restaurantes perto de você</Text>
-
-      <UserCarouselRestaurant variant="closeToYou" />
-
-    </View>
     </ScrollView>
-  ); 
+  );
 }
 
 const styles = StyleSheet.create({
@@ -59,7 +91,7 @@ const styles = StyleSheet.create({
   inputWrapper: {
     flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: '#FFF1E6', 
+    backgroundColor: '#FFF1E6',
     borderRadius: 999,
     paddingHorizontal: 16,
     paddingVertical: 12,
@@ -88,10 +120,16 @@ const styles = StyleSheet.create({
     shadowRadius: 4,
     elevation: 5,
   },
+  roulleteIcon: {
+    width: 30,
+    height: 30,
+  },
+  
   title: {
     fontSize: 18,
     fontWeight: 'bold',
     color: Colors.orange.orangeBold,
+    marginTop: 20,
+    paddingHorizontal: 20,
   },
-
 });
