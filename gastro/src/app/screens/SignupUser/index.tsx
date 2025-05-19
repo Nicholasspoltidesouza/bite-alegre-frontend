11b16053e260c9fa87bbd042a250f03f318c27b7
gabriellrmartins <gabriellrmartins@gmail.com>
2025-05-19 16:42:57 -0300

    adicionando botão ToggleSwitch para IsInfluencer no SignupUser

--- gastro/src/app/screens/SignupUser/index.tsx
+++ gastro/src/app/screens/SignupUser/index.tsx
@@ -3,6 +3,7 @@ import Button from '@/src/components/Button';
 import Dropdown from '@/src/components/Dropdown';
 import SignupHeader from '@/src/components/SignupHeader';
 import CustomTextInput from '@/src/components/TextFieldCadastroUsuario';
+import ToggleSwitch from '@/src/components/ToggleSwitch'; // Importar o ToggleSwitch
 import Colors from '@/src/constants/Colors';
 import { useCreateUser } from '@/src/hooks/useUserApi';
 import { useRouter, useLocalSearchParams } from 'expo-router';
@@ -46,6 +47,9 @@ const SignupUser = () => {
   );
   const [userType, setUserType] = useState<string | null>(
     userData ? JSON.parse(userData as string).userType : 'Cadastro de Usuário',
+  );
+  const [isInfluencer, setIsInfluencer] = useState<boolean>(
+    userData ? JSON.parse(userData as string).isInfluencer || false : false,
   );
 
   const [birthDateTouched, setBirthDateTouched] = useState<boolean>(false);
@@ -164,6 +168,7 @@ const SignupUser = () => {
       gender: formattedGender,
       birthDate: isoBirthDate,
       userType: formattedUserType,
+      isInfluencer,
     };
 
     router.push({
@@ -310,6 +315,16 @@ const SignupUser = () => {
               )}
             </View>
           </View>
+
+          <View style={styles.influencerToggleContainer}>
+            <Text style={[styles.influencerLabel, { marginRight: 10 }]}>Sou Influenciador</Text>
+            <ToggleSwitch
+              isEnabled={isInfluencer}
+              onToggle={setIsInfluencer}
+              activeColor={Colors.orange.orangeStandard}
+            />
+          </View>
+
           <View style={styles.buttonContainer}>
             <Button
               title="Avançar"
@@ -382,6 +397,17 @@ const styles = StyleSheet.create({
     width: '90%',
     flexDirection: 'row',
     justifyContent: 'flex-end',
+  },
+  influencerToggleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '90%',
+    marginBottom: '5%',
+  },
+  influencerLabel: {
+    fontSize: 16,
+    color: Colors.orange.orangeStandard,
+    fontFamily: 'Poppins-Regular',
   },
 });
 
