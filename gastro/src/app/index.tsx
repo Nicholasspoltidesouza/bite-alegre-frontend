3639692914d82a6bba9940696b548a1b283ad72f
Isabella Becker <isabellalbecker@gmail.com>
2025-05-20 20:09:09 -0300

    feat: desfiz as mudanças da app

--- gastro/src/app/index.tsx
+++ gastro/src/app/index.tsx
@@ -4,33 +4,45 @@ import { StyleSheet, View } from 'react-native';
 import Button from '../components/Button';
 import Header from '../components/Header';
 import Colors from '../constants/Colors';
-import { useAuthApi } from '../hooks/useAuthApi';
-import { ReviewDTO, UserDTO } from '../@types/DTO';
-import { CardReview } from '../components/ReviewCard';
 
 const App: React.FC = () => {
-  const { login } = useAuthApi();
-  const review = {
-    stars: 4,
-    feedback: "Lorem ipsum dolor sit amet. Ad similique dicta et modi molestiae sed dolorem voluptatum et voluptatibus fugit ea ullam necessitatibus vel illo consequatur At necessitatibus sunt. Qui quasi velit ut perferendis quia sed placeat nisi 33 facere rerum aut omnis quia qui veritatis dolore? Aut consectetur quia ",
-    restaurantName: "Pedrinho Lanches",
-    restaurantProfilePhoto:"https://static.ifood-static.com.br/image/upload/t_medium/logosgde/a4d28d60-aa8b-483f-911f-ac8bb7c670d3/202304261653_V81M.png",
-    restaurantId:'rest-2'
-  } as ReviewDTO
-
-
-  function handle() {
-    const user = {
-      email: 'user1@bite.io',
-      password: 'user1'
-    } as UserDTO
-    login(user);
-  }
-
   return (
     <View style={styles.container}>
       <Header name={'Manu'} nickName={'manu'} />
-      <CardReview review={review}/>
+      <View style={styles.buttons}>
+        <Button
+          title="Profile"
+          onPress={() => router.push({ pathname: '/screens/Profile' })}
+          type={'orange'}
+        />
+        <Button
+          title="Perfil Restaurante"
+          onPress={() =>
+            router.push({ pathname: '/screens/restaurantProfile' })
+          }
+          type={'orange'}
+        />
+        <Button
+          title="Cadastro Usuário"
+          onPress={() => router.push({ pathname: '/screens/SignupUser' })}
+          type={'orange'}
+        />
+        <Button
+          title="Cadastro Restaurante"
+          onPress={() => router.push({ pathname: '/screens/SignupRestaurant' })}
+          type={'orange'}
+        />
+        <Button
+          title="Feed"
+          onPress={() => router.push({ pathname: '/screens/Feed' })}
+          type={'orange'}
+        />
+        <Button
+          title="Adicionar Mídia"
+          onPress={() => router.push({ pathname: '/screens/AddMedia' })}
+          type={'orange'}
+        />
+      </View>
     </View>
   );
 };
