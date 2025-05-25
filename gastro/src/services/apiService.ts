9b37323c462979aca276d7dda6ceb349a6cf9fb5
Isabella <isabellalbecker@gmail.com>
2025-05-25 10:52:59 -0300

    add patch no cors

--- gastro/src/services/apiService.ts
+++ gastro/src/services/apiService.ts
@@ -1,5 +1,5 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { API_URL_ANDROID } from "../constants/apiUrl";
+import { API_URL_ANDROID, API_URL_BACKEND } from "../constants/apiUrl";
 
 export interface ApiErrorResponse {
     error?: string;
@@ -8,7 +8,7 @@ export interface ApiErrorResponse {
 
 class ApiService {
     private baseUrl: string;
-    private API_URL = API_URL_ANDROID;
+    private API_URL = API_URL_BACKEND;
 
     constructor(baseUrl: string) {
         if (!baseUrl) {
