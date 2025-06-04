22f2af6fbb874ef221a4383c43cb39f4e11fe68b
Nicholas Spolti <nicholasspoltidesouza@gmail.com>
2025-06-04 15:51:18 -0300

    feat: maestro tester

--- gastro/src/app/Home/index.tsx
+++ gastro/src/app/Home/index.tsx
@@ -20,7 +20,7 @@ const Home: React.FC = () => {
                     resizeMode="contain"
                 />
 
-                <View style={styles.textContainer}>
+                <View testID="home" style={styles.textContainer}>
                     <Text style={styles.text}>
                         Primeira vez aqui?{" "}
                         <Text style={styles.bold} onPress={() => router.push({ pathname: "/SignupUser" })}>
@@ -37,7 +37,7 @@ const Home: React.FC = () => {
 
                     <Text style={styles.text}>
                         Já possui uma conta?{" "}
-                        <Text style={styles.bold} onPress={() => router.push({ pathname: "/Login" })}>
+                        <Text testID="entrar" style={styles.bold} onPress={() => router.push({ pathname: "/Login" })}>
                             Entrar
                         </Text>
                     </Text>
