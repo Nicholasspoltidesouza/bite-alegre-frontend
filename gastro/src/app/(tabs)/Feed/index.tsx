1e9dbe5d98bfc4035ef21a3b0d379eabda39e745
Isabella Becker <isabellalbecker@gmail.com>
2025-06-09 19:00:52 -0300

    feat: add titulo

--- gastro/src/app/(tabs)/Feed/index.tsx
+++ gastro/src/app/(tabs)/Feed/index.tsx
@@ -94,6 +94,8 @@ export default function Feed() {
 
         <Text style={styles.title}>Restaurantes perto de você</Text>
         <UserCarouselRestaurant variant="closeToYou" items={restaurants} />
+        
+        <Text style={styles.title}>Influeciadores para de você</Text>
         <UserCarouselRestaurant
           variant="influencers"
           items={feedData?.publications.map(mapPublicationToCarouselItem) ?? []}
